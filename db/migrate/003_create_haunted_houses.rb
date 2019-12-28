61c552a8649a5c5ee5c2119228daca8614b00596
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-28 20:51:42 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.name :string
+      t.location :string
+      t.theme :string
+      t.price :integer
+      t.family_friendly? :boolean
+      t.opening_date :string
+      t.closing_date :string
+      t.full_description :string
+    end
+  end
+end
